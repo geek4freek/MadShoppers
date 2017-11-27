c1ca0645c968b9ab065da209a73a7340b4ee9f12
geek4freek <33922047+geek4freek@users.noreply.github.com>
2017-11-27 18:08:15 +0530

    bundle

--- MadShoppers/App_Start/BundleConfig.cs
+++ MadShoppers/App_Start/BundleConfig.cs
@@ -8,32 +8,39 @@ namespace MadShoppers
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/Site.css")
+                .Include("~/Content/vendor/bootstrap/css/bootstrap.css")
+                 .Include("~/Content/vendor/bootstrap/css/bootstrap.min.css")
+                 .Include("~/Content/vendor/bootstrap/css/bootstrap-grid.css")
+                 );
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/jquery-easing/jquery.easing.min.js",
+                "~/Content/vendor/magnific-popup/jquery.magnific-popup.min.js"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-1.10.2.js",
                         "~/Content/js/jqBootstrapValidation.js",
                         "~/Content/js/contact_me.js",
                         "~/Content/js/freelancer.min.js"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.JS"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.6.2.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Content/vendor/jquery/jquery.min.js",
-                      "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
-                      "~/Content/vendor/jquery-easing/jquery.easing.min.js",
-                      "~/Content/vendor/magnific-popup/jquery.magnific-popup.min.js",
-                      "~/Content/vendor/bootstrap/css/bootstrap.min.css"));
+      
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+       
         }
     }
 }
