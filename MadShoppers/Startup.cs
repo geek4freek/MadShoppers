f79f68872ef764c3b94a54f7579a8db2ee4c6526
geek4freek <33922047+geek4freek@users.noreply.github.com>
2017-11-27 14:53:16 +0530

    f4

---
+++ MadShoppers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MadShoppers.Startup))]
+namespace MadShoppers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
